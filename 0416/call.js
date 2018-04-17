6c0ce991bf57fdae5d4a969bfed8626b75295b0b
xiexuanzheng123 <349328984@qq.com>
2018-04-17 14:25:36 +0800

    use call to extend

---
+++ 0416/call.js
@@ -0,0 +1,35 @@
+function Animal(name) {
+    this.name = name || 'Animal';
+    this.sleep = function() {
+        console.log(this.name + '正在睡觉');
+    }
+}
+Animal.prototype.eat = function(food) {
+    console.log(this.name + ' 正在吃：' + food);
+}
+
+
+/**
+ * 使用父类的构造函数来增强子类实例，等于是赋值父类的实例属性给子类（没有用到原型）、
+ * 特点：
+ * 1 解决了1中，子类实例共享父类引用属性的问题
+ * 2 创建子类实例时，可以向父类传递参数
+ * 3 可以实现多继承
+ * 
+ * 缺点：
+ * 1 实例不是父类的实例，只是子类的实例
+ * 2 只能继承父类的实例属性和方法，不能继承原型属性和方法
+ * 3 无法实现函数复用，每个子类都有父类实例函数的副本
+ * 
+ */
+
+function Cat(name) {
+    Animal.call(this);
+    this.name = name || 'Tom';
+}
+
+var cat = new Cat();
+console.log(cat.name);
+console.log(cat.sleep());
+console.log(cat instanceof Animal);
+console.log(cat instanceof Cat);
