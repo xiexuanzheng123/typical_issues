ef366c4099de77b093ca5f2329c99e4fdf0751dc
xiexuanzheng123 <349328984@qq.com>
2018-04-23 15:09:39 +0800

    position

---
+++ 0423/index.js
@@ -0,0 +1,20 @@
+/**http://web.jobbole.com/94152/?utm_source=blog.jobbole.com&utm_medium=relatedPosts
+ * 50道css试题
+ * 
+ * 一 display 有哪些
+ *  1 inline
+ *  2 block
+ *  3 inline-block
+ *  4 table
+ *  5 table-cell
+ *  6 none
+ *  7 list-item
+ * 
+ * 二 position的值
+ *  1 static 默认的没有定位，LTRB无效
+ *  2 relative：不脱离文档流，相对于自身默认位置的定位，LTRB有效
+ *  3 absolute：漂出文档流，参考最近一个不是static的父级元素定位，LTRB有效
+ *  4 fixed 
+ * 
+ * 
+ */
